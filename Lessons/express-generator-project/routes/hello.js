a512067dbc0b51c57aa7c29f5c74d1be2ef6826b
Ahsan <m.ahsan9981@gmail.com>
2022-01-31 20:39:33 +0500

    templating.

--- Lessons/express-generator-project/routes/hello.js
+++ Lessons/express-generator-project/routes/hello.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const crypto = require('crypto');
 
-router.get('/hello', (req, res, next) => {
-
+router.get('/', (req, res, next) => {
     const name = req.query.name;
-
     res.render('hello', { name });
+});
+
+router.get('/time', (req, res, next) => {
+    res.json({
+        time: new Date().toISOString()
+    })
+});
+
+router.post('/hash', (req, res, next) => {
+
+    const plainText = req.body.plainText;
+    const hash = crypto.createHash('md5').update(plainText).digest('hex');
+
+    res.json({
+        plainText,
+        hash
+    });
+});
+
+router.get('/:person/:greetings', (req, res, next) => {
+    const name = req.params.person;
+    const greetings = req.params.greetings;
+    res.render('hello', { name, greetings });
 });
 
 module.exports = router;
--- Lessons/express-generator-project/routes/index.js
+++ Lessons/express-generator-project/routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/ejs', function(req, res, next) {
+
+  res.render('ejs-example', { 
+    title: '<h1>Example EJS Template</h1>',
+    showTitle: true,
+    data: ['apple', 'oranges', 'peaches', 'mangos', 'grapes']
+  });
+
+});
+
 module.exports = router;
